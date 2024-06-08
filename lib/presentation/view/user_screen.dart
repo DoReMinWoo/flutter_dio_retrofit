7ba625047f64784859f00e061dccc9a027e09826
doreminwoo <kimy5878@naver.com>
2024-06-08 19:02:00 +0900

    feat : User Screen

---
+++ lib/presentation/view/user_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dio_retrofit/presentation/provider/user_provider.dart';
+import 'package:flutter_dio_retrofit/util/logger.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class UserScreen extends ConsumerStatefulWidget {
+  const UserScreen({super.key});
+
+  @override
+  UserScreenState createState() => UserScreenState();
+}
+
+class UserScreenState extends ConsumerState<UserScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final userProviderResult = ref.watch(userProvider);
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Riverpod Example'),
+        ),
+        body: Center(
+          child: userProviderResult.when(
+            loading: () => const CircularProgressIndicator(),
+            data: (reqresModelResponse) {
+              final users = reqresModelResponse.data;
+              logger.d(users.toString());
+              return ListView.builder(
+                itemCount: users.length,
+                itemBuilder: (context, index) {
+                  final user = users[index];
+                  return ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage(user.avatar!),
+                    ),
+                    title: Text('${user.firstName} ${user.lastName}'),
+                    subtitle: Text(user.email),
+                  );
+                },
+              );
+            },
+            error: (error, stackTrace) {
+              return Text('Error: $error');
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}

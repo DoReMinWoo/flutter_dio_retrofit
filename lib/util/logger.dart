49346814fbcdd1db308f0bcca544fbe116f20215
doreminwoo <kimy5878@naver.com>
2024-06-09 13:26:49 +0900

    feat : 릴리즈모드에서 log 비활성화

--- lib/util/logger.dart
+++ lib/util/logger.dart
@@ -1,5 +1,11 @@
+import 'package:flutter/foundation.dart';
 import 'package:logger/logger.dart';
 
-Logger log = Logger(
-  printer: PrettyPrinter(),
-);
+Logger log = kDebugMode
+    ? Logger(
+        printer: PrettyPrinter(),
+      )
+    : Logger(
+        printer: PrettyPrinter(),
+        level: Level.off,
+      );
